b71183fc192a8aa58032a3dfaa87809c6b959858
yuaefimoff <alice.efimoff@gmail.com>
2025-02-19 22:42:38 -0500

    Minor fixes

--- minStartingIndex.java
+++ minStartingIndex.java
@@ -1,6 +1,8 @@
+
 import java.util.*;
 
 class minStartingIndex {
+
     public int minStartingIndex(String s, String pattern) {
         // Blind approach: This is a slightly modified version of a string matching algorithm
         // Since the constraints use the alphabet, I will use a Boyer-Moore string matching approach
@@ -15,41 +17,30 @@ class minStartingIndex {
         int almostCnt = 0;
 
         System.out.println("" + s + "\n" + pattern);
-        while (i < s.length() && j >= 0) { 
+        while (i < s.length() && j >= 0) {
             System.out.println(i + " to " + j);
-            /** 
-            if (s.charAt(i) == pattern.charAt(j)) {
-                i--;
-                j--;
-            } else{
-                
-                almostCnt++;
-                System.out.println("Mistakes incremented to " + almostCnt + " (" + s.charAt(i) + " != " + pattern.charAt(j) + ")");
-                
-            }*/
             if (s.charAt(i) != pattern.charAt(j)) {
                 almostCnt++;
                 System.out.println("MISTAKE #" + almostCnt + " (" + s.charAt(i) + " != " + pattern.charAt(j) + ")");
-            }
+                if (almostCnt > 1) {
+                    System.out.println("Last occurence at s[i] = " + s.charAt(i) + ": " + arrL[s.charAt(i) - 'a']);
+                    System.out.println("Suffix skip for pattern[j] " + arrS[j]);
+                    System.out.println("i:" + i + "\tj:" + j + "\tSelected: " + Math.min(arrL[s.charAt(i) - 'a'], arrS[j]));
 
-            if (almostCnt > 1) {
-                System.out.println("Last occurence at s[i] = " + s.charAt(i) + ": " + arrL[s.charAt(i) - 'a']); 
-                System.out.println("Suffix skip for pattern[j] " + arrS[j]); 
-                System.out.println("i:" + i + "\tj:" + j + "\tSelected: " + Math.min(arrL[s.charAt(i) - 'a'], arrS[j])); 
-                
-                i = i + pLen - Math.min(arrL[s.charAt(i) - 'a'], arrS[j]);
-                System.out.println("NEW INDEX: " + i + "\t FURTHEST INDEX: " + (s.length() - 1)); 
-                j = pLen;
-                almostCnt = 0;
-                continue;
+                    i = i + pLen - Math.min(arrL[s.charAt(i) - 'a'], arrS[j]);
+                    System.out.println("NEW INDEX: " + i + "\t FURTHEST INDEX: " + (s.length() - 1));
+                    j = pLen;
+                    almostCnt = 0;
+                }
+                i--;
+                j--;
+            } else {
+                i--;
+                j--;
             }
-            i--;
-            j--;
-            
         }
 
-        return (j == -1)?(i + 1):-1;
-
+        return (j == -1) ? (i + 1) : -1;
     }
 
     private int[] lastOccurence(String p) {
@@ -57,8 +48,10 @@ class minStartingIndex {
         int[] lastOcc = new int[alphabet.length];
 
         Arrays.fill(lastOcc, -1);
-        for (int i = 0; i < p.length(); i++) lastOcc[p.charAt(i) - 'a'] = i;
-        System.out.println("Last occurence: \n" +Arrays.toString(lastOcc));
+        for (int i = 0; i < p.length(); i++) {
+            lastOcc[p.charAt(i) - 'a'] = i;
+        }
+        System.out.println("Last occurence: \n" + Arrays.toString(lastOcc));
         return lastOcc;
     }
 
@@ -73,8 +66,8 @@ class minStartingIndex {
 
             // Find largest j such that P[i+1..m-1] = P[j+1..j+m-1-i] and P[i] != P[j]
             for (int j = i - 1; j >= 0; j--) {
-                if (pattern.charAt(i) != pattern.charAt(j) 
-                && pattern.substring(i+1, m).equals(pattern.substring(j+1, j + m - i))) {
+                if (pattern.charAt(i) != pattern.charAt(j)
+                        && pattern.substring(i + 1, m).equals(pattern.substring(j + 1, j + m - i))) {
                     suffixSkip[i] = j; // Set S[i] to the valid j
                     found = true;
                     break;
@@ -86,19 +79,21 @@ class minStartingIndex {
                 //suffixSkip[i] = i - (m - 2);
                 int j = 1;
                 for (j = 1; i + 1 + j < m; j++) {
-               
-                //System.out.println("Suffix: " + pattern.substring(i + 1 + j) + "\tPrefix: " + pattern.substring(0, m - (i + 1) - j));
-                if (pattern.substring(i + 1 + j).equals(pattern.substring(0, (m - (i + 1) - j)))) {
-                    //System.out.println("Prefix and suffix math! Assigning value " + -j);
-                    suffixSkip[i] = -j;
-                    found = true;
-                    break;
+
+                    //System.out.println("Suffix: " + pattern.substring(i + 1 + j) + "\tPrefix: " + pattern.substring(0, m - (i + 1) - j));
+                    if (pattern.substring(i + 1 + j).equals(pattern.substring(0, (m - (i + 1) - j)))) {
+                        //System.out.println("Prefix and suffix math! Assigning value " + -j);
+                        suffixSkip[i] = -j;
+                        found = true;
+                        break;
+                    }
                 }
-               }
-               if(!found) suffixSkip[i] = -j ;
+                if (!found) {
+                    suffixSkip[i] = -j;
+                }
             }
         }
-        System.out.println("Suffix skip: \n" +Arrays.toString(suffixSkip));
+        System.out.println("Suffix skip: \n" + Arrays.toString(suffixSkip));
         return suffixSkip;
     }
 
@@ -108,13 +103,12 @@ class minStartingIndex {
         String s1 = "ababbababa";
         String pattern1 = "bacaba";
         int result1 = solution.minStartingIndex(s1, pattern1);
-        System.out.println("Test case 1: " + result1 + "\n"); // Expected output: 10
+        System.out.println("Test case 1: " + result1 + "\n");
 
-        
         String s2 = "abcdefg";
         String pattern2 = "bcdffg";
         int result2 = solution.minStartingIndex(s2, pattern2);
-        System.out.println("\nTest case 2: " + result2); // Expected output: 10 
+        System.out.println("\nTest case 2: " + result2);
 
     }
 }
